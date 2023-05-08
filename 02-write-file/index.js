6a145f7a22711c26fdb7dc5c5c6ed9511605e85a
Dolgor <budatsyrenova.dolgor@mail.ru>
2023-05-09 03:03:06 +0800

    feat:update 01-read-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const { stdin,stdout } = process;
+stdout.write('Введите текст'+"\n");
+stdin.on('data', (data) => {
+    if(data.toString()=='exit\r\n') process.exit();
+    output.write(data);
+});
+process.on('exit',()=>stdout.write('Пока!'));
+process.on('SIGINT', () => { process.exit()});
